33163ce8236b98819ee0431a0d868c3eaefa5c0e
Christián Feliks <christian.feliks@student.tuke.sk>
2023-02-17 16:17:53 +0100

    done excercies 01, 02, 03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(word, repeat) {
 
+   if(repeat < 0) {
+      return 'ERROR';
+   }
+   
+   let new_word = "";
+   for (let i = 0; i < repeat; i++){
+      new_word = new_word + word;
+   }
+   
+   return new_word;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,18 @@
-const reverseString = function() {
+const reverseString = function(str) {
+   let result = "";
+   let split_arr = str.split(' ');
 
+   for(let i = split_arr.length-1; i >= 0; i--){
+      let word = split_arr[i];
+      for(let j = (word.length - 1); j >= 0; j--){
+
+         result += word[j];
+      }
+      if (split_arr.length > 1 && i != 0){
+         result += ' ';
+      }
+   } 
+   return result;
 };
 
 // Do not edit below this line
