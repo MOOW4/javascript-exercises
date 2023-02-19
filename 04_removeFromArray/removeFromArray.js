99e20fa8b2420ae88bdd6d4cb12e63e9dcb30980
Christián Feliks <christian.feliks@student.tuke.sk>
2023-02-19 01:00:58 +0100

    add completed 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, remove) {
+   //array.splice(1,1)
+   let args = Array.from(arguments);
+
+   for(let j = 1; j < args.length; j++){
+      for(let i = 0; i < arr.length; i++){
+         if(arr[i] === args[j]){
+            arr.splice(i,1);
+         }
+      }
+   }
+   return arr;
 
 };
 
