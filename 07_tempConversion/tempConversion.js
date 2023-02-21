26fa8975c9d500bbb37062100bb398df24aaadde
Christián Feliks <christian.feliks@student.tuke.sk>
2023-02-21 21:20:45 +0100

    add finished excercises 05;06;07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(faren) {
+  let cels = (faren - 32) * (5/9);
+  //Math.round(cels * 100) / 100;
+  return (+cels.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cels) {
+  let faren = cels * (9/5) + 32;
+  //Math.round(faren * 10) / 10;
+  return (+faren.toFixed(1));
 };
 
 // Do not edit below this line
