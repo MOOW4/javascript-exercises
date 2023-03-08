f6ff182075424db2a17748fc94e3d64ffa2e228f
Christián Feliks <christian.feliks@student.tuke.sk>
2023-03-08 15:12:54 +0100

    done 10 fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(num < 0){
+        return "OOPS"
+    }
+    let x = 1;
+    let y = 1;
+    let temp = 0;
+    for(let i = 0; i < num - 2; i++){
+        temp = y;
+        y = x + temp;
+        x = temp;
+    }
+    return y;
 };
 
 // Do not edit below this line
