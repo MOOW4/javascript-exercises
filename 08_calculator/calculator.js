94b12d3022b0a4296dc8712b8f554f9faa8a9623
Christián Feliks <christian.feliks@student.tuke.sk>
2023-03-08 14:13:24 +0100

    done calculator exc

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(number_1, number_2) {
+    let sum = number_1 + number_2;
+	  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(number_1, number_2) {
+  let diff = number_1 - number_2;
+  return diff;
 };
 
-const sum = function() {
-	
+const sum = function(num_array) {
+
+  let sum = 0;
+
+  for(let i = 0; i < num_array.length; i++){
+    sum += num_array[i];
+  }
+
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(num_array) {
+  let result = 1;
+  for(let i = 0; i < num_array.length; i++){
+    result *= num_array[i];
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	let result = base ** exponent;
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let result = 1;
+  for(let i = num; i > 0; i--){
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
